02676a4195e826f7ea0da1524d7b636fa99801a4
Gaeun-Kang <53822816+Gaeun-Kang@users.noreply.github.com>
2024-04-14 14:26:18 +0900

    오류 해결
    오류오류

--- Source/CRTest/Item/AdventureItem/CRAdventureMeatItem.cpp
+++ Source/CRTest/Item/AdventureItem/CRAdventureMeatItem.cpp
@@ -3,9 +3,10 @@
 
 #include "Item/AdventureItem/CRAdventureMeatItem.h"
 #include "Item/AdventureItem/CRAdventureItemData.h"
-
+#include "Components/BoxComponent.h"
 ACRAdventureMeatItem::ACRAdventureMeatItem()
 {
+	Trigger->SetBoxExtent(FVector(100.0f, 100.0f, 100.0f));	
 	static ConstructorHelpers::FObjectFinder<UCRAdventureItemData> ItemDataRef(TEXT("/Game/CosmicRestaurant/Data/DA_MeatItem.DA_MeatItem"));
 	if (ItemDataRef.Object)
 	{
